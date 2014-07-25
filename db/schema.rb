083c5dce600c217154f262dd50d50ecd05928154
Nathanial L. McConnell <nate@synaptian.com>
2014-07-25 11:02:58 -0400

    adding media_contents controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140725150132) do
+
+  create_table "media", force: true do |t|
+    t.string   "file_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
